01685ce3d9a5e1e981bd530498ffbc27c52e107d
Haris Razis <xaris.ws98@gmail.com>
2020-03-16 14:16:26 +0200

    0.1.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,32 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 20 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
+//  Default state of nav and container
+if ($(window).scrollTop() > 50) {
+    $(".navbar").addClass("active");
+};
+
+if ($(window).scrollTop() > 100) {
+    $("#overlap-container").addClass("moveup");
+};
+
+//  Navbar - change color
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+        $(".navbar").addClass("active");
+    } else {
+        $(".navbar").removeClass("active");
+    }
+});
+
+//  Container movement
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        $("#overlap-container").addClass("moveup");
+    } else {
+        $("#overlap-container").removeClass("moveup");
+    }
+});
+
+//  Carousel options
+$('.carousel').carousel({
+    interval: 3000,
+    wrap: true
 });
