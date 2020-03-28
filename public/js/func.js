7da427f536a4a83b10e31a9c42551373dec8f854
xrazis <xaris.ws98@gmail.com>
2020-03-28 17:49:33 +0200

    Change logo color on scroll

--- public/js/func.js
+++ public/js/func.js
@@ -1,53 +1,55 @@
 // Scroll to top button appear
 $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $(".scroll-to-top").fadeIn();
-    } else {
-      $(".scroll-to-top").fadeOut();
-    }
-  });
-  
-  //  Default state of nav and container
+  var scrollDistance = $(this).scrollTop();
+  if (scrollDistance > 100) {
+    $(".scroll-to-top").fadeIn();
+  } else {
+    $(".scroll-to-top").fadeOut();
+  }
+});
+
+//  Default state of nav and container
+if ($(window).scrollTop() > 50) {
+  $(".navbar").addClass("active");
+  $(".logo-main").attr("src", "img/logos/logo-main_black.png");
+}
+
+if ($(window).scrollTop() > 60) {
+  $("#overlap-container").addClass("moveup");
+}
+
+//  Navbar - change color
+$(window).scroll(function() {
   if ($(window).scrollTop() > 50) {
     $(".navbar").addClass("active");
+    $(".logo-main").attr("src", "img/logos/logo-main_black.png");
+  } else {
+    $(".navbar").removeClass("active");
+    $(".logo-main").attr("src", "img/logos/logo-main.png");
   }
-  
+});
+
+//  Container movement
+$(window).scroll(function() {
   if ($(window).scrollTop() > 60) {
     $("#overlap-container").addClass("moveup");
+  } else {
+    $("#overlap-container").removeClass("moveup");
   }
-  
-  //  Navbar - change color
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 50) {
-      $(".navbar").addClass("active");
-    } else {
-      $(".navbar").removeClass("active");
-    }
-  });
-  
-  //  Container movement
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 60) {
-      $("#overlap-container").addClass("moveup");
-    } else {
-      $("#overlap-container").removeClass("moveup");
-    }
-  });
-  
-  //  Carousel options
-  $(".carousel").carousel({
-    interval: 3000,
-    wrap: true
-  });
-  
-  //  Fullscreen menu on small/medium screens
-  $("#navToggler").click(function() {
-    $("#navbarSupportedContent")
-      .stop(true, false)
-      .animate({ width: "toggle" }, 350);
-    $(this).toggleClass("move-toggler", 300);
-    $("#changeIcon").toggleClass("changeIcon", 300);
-    $("#logo").toggle("drop", 500);
-  });
-  
+});
+
+//  Carousel options
+$(".carousel").carousel({
+  interval: 3000,
+  wrap: true
+});
+
+//  Fullscreen menu on small/medium screens
+$("#navToggler").click(function() {
+  $("#navbarSupportedContent")
+    .stop(true, false)
+    .animate({ width: "toggle" }, 350);
+  $(this).toggleClass("move-toggler", 300);
+  $("#changeIcon").toggleClass("changeIcon", 300);
+  $("#logo").toggle("drop", 500);
+});
